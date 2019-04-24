e7aed5825cd75583c93681678c3675bf9ebfa1d4
Neil Bardhan <indraneil.bardhan@gmail.com>
2019-04-24 10:40:59 -0400

    declaring and initializing variables in golang

---
+++ 1variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var deckSize int //Variable can be declared outside func main
+
+func main(){
+  var x string = "This throws a warning" //because go can infer datatype from rhs, 'string' is redundant in var declaration
+  a := "scoopity"
+  fmt.Println(a)
+  card := "zippity zoppity"
+  //card = 7 This is an error, go is a statically typed language. Var types cannot be changed after declaration
+	fmt.Println(card)
+	card2 := 36.57 //Initialization of 'card2' NOTE : ':=' is only used when a var is declared and initialized 
+	card2 = 358.75 //Reassignment of 'card2 note: reassignment is '=' operator
+	fmt.Println(card2)
+	deckSize = 52 //Previously declared, now being initialized. NOTE: '=' is used here because var was declared previously, now only value is being assigned
+	fmt.Println(deckSize)
+ }
