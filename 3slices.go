6509ab8551cd77f7d2e4a3f4d6d7898ca4728fda
Neil Bardhan <indraneil.bardhan@gmail.com>
2019-04-24 21:42:44 -0400

    slices in go

--- 3slices.go
+++ 3slices.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{newCard(), "5D", newCard()} //a slice can only contain elements of 1 pre-defined type
-	cards = append(cards, "QD") //a slice can be updated (elements added and removed), arrays are fixed length
-	fmt.Println(cards) //like py lists, a slice can be printed all at once
+	cards := []string{newCard(), "5D", newCard()}
+	cards = append(cards, "QD")
+	fmt.Println("Cards : ", cards)
 
-  //can also print them itemwise
 	for i, card := range cards { //for loops in go, discard 'i' and 'card' values after each iteration
-		fmt.Println(i, card)
+		fmt.Println("Index : ", i, "||", "Card : ", card)
 	}
 
 	colors := []string{"Red", "Yellow", "Blue"}
 	for col := range colors { //'col' is index, if we don't use 'i'
-		fmt.Println(colors[col]) //call element of 'colors' using 'col'
+		fmt.Println("Color : ", colors[col]) //call element of 'colors' using 'col'
 	}
 }
 
