8cfa23bc05fee1c4ee9b2c791b63f5769cde5c5a
Neil Bardhan <indraneil.bardhan@gmail.com>
2019-05-02 09:59:26 -0400

    added dealing of new hand

--- cards_project/main.go
+++ cards_project/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	start := time.Now()
 	cards := newDeck()
+	fmt.Println("+------------ALL CARDS------------+")
 	cards.print() //Calls the print method from deck
+	hand, remainingDeck := deal(cards, 5)
+	//A hand of size 5 and the remaining crds are returned. Both of type 'deck'
+	fmt.Println("+------------CHOSEN HAND------------+")
+	hand.print()
+	fmt.Println("+------------REMAINING DECK------------+")
+	remainingDeck.print()
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Println("Time elapsed : ", elapsed)
 }
